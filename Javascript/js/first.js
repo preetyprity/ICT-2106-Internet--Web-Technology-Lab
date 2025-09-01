639dd15217f233bfe9e2b175966180a01b1cab9d
preetyprity <pritydaskuttisuna@gmail.com>
2025-09-01 17:25:41 +0600

    up

--- Javascript/js/first.js
+++ Javascript/js/first.js
@@ -60,3 +60,14 @@ console.log("a=",a,"b=",b);
  console.log("a%b=",a%b);
  console.log("a**b=",a**b);
 
+//unary operator
+let c=3;
+let d=8;
+
+console.log("c=",c,"&d=",d);
+c++;
+console.log("c=",c);
+
+console.log("a==b:?",a==b);
+
+

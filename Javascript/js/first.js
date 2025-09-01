5fe28cb2a89d3e36340c6ce2be1f323557619303
preetyprity <pritydaskuttisuna@gmail.com>
2025-09-01 17:13:50 +0600

    up

---
+++ Javascript/js/first.js
@@ -0,0 +1,62 @@
+console.log("prity das");
+console.log("welcomne");
+fulname="pritom";
+console.log(fulname);
+price=57.87;
+console.log(price);
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
+isfollow=true;
+console.log(isfollow);
+let name="ankon";
+console.log(name);
+
+//object(student)
+const student = {
+
+fullName:"prity",
+age:20,
+cgpa:8.2,
+ispass:true,
+
+};
+console.log(student.age);
+
+student["age"]=student["age"]+1;
+console.log(student.age);
+
+const product = {
+
+tittle:"Ball pen",
+rating:4,
+offer:5,
+price:67,
+};
+
+console.log(product);
+
+
+const Prity={
+
+userName:"prity das",
+posts:46,
+followers:56,
+following:4,
+Email:"pritydaskuttisuna@gamil.com"
+
+};
+console.log(Prity);
+//Arithemetic operators
+let a=5;
+let b=2;
+
+console.log("a=",a,"b=",b);
+ console.log("a+b=",a+b);
+ console.log("a-b=",a-b);
+ console.log("a*b=",a*b);
+ console.log("a/b=",a/b);
+ console.log("a%b=",a%b);
+ console.log("a**b=",a**b);
+
